1e9a32a9e083640f4875644d725f97592a4c69b4
Micah Mutrux <micah.mutrux@gmail.com>
2019-06-12 22:46:17 -0400

    update content

--- src/shared/dishes.js
+++ src/shared/dishes.js
@@ -2,12 +2,12 @@ const DISHES =
 	[
 		{
 			id: 0,
-			name:'Uthappizza',
-			image: 'assets/images/uthappizza.png',
+			name:'Code for BTV',
+			image: 'assets/images/cfbtv_bg.jpg',
 			category: 'mains',
 			label:'Hot',
-			price:'4.99',
-			description:'A unique combination of Indian Uthappam (pancake) and Italian pizza, topped with Cerignola olives, ripe vine cherry tomatoes, Vidalia onion, Guntur chillies and Buffalo Paneer.',
+			website:'http://www.codeforbtv.org',
+			description:'Many of our most important institutions suffer from technology problems that can be solved with relatively simple solutions, and that\'s what Code for BTV does. Think of Code for BTV as a pro-bono digital agency.  We pull together teams of volunteers that build new tools for social and civic justice organizations so their hard work goes further.',
 			comments: [
 				{
 					id: 0,
@@ -47,12 +47,12 @@ const DISHES =
 			]                        },
 		{
 			id: 1,
-			name:'Zucchipakoda',
-			image: 'assets/images/zucchipakoda.png',
+			name:'Neighbor Helping Neighbor',
+			image: 'assets/images/nhn_bg.jpg',
 			category: 'appetizer',
 			label:'',
-			price:'1.99',
-			description:'Deep fried Zucchini coated with mildly spiced Chickpea flour batter accompanied with a sweet-tangy tamarind sauce',
+			website:'https://www.communitychurchofhuntington.org/neighbor-helping-neighbor-nhn1.html',
+			description:'NHN is our community-based safety net for children, families and seniors in the greater Huntington area. Your tax deductible donation helps people who are struggling with immediate financial crisis such as the inability to pay for fuel, electricity, food or other essential basic needs.',
 			comments: [
 				{
 					id: 0,
@@ -93,12 +93,12 @@ const DISHES =
 		},
 		{
 			id: 2,
-			name:'Vadonut',
-			image: 'assets/images/vadonut.png',
+			name:'Echo Center',
+			image: 'assets/images/echo_bg.jpg',
 			category: 'appetizer',
 			label:'New',
-			price:'1.99',
-			description:'A quintessential ConFusion experience, is it a vada or is it a donut?',
+			website:'https://www.echovermont.org',
+			description:'ECHO, Leahy Center for Lake Champlain, formerly the Lake Champlain Basin Science Center, is an innovative science and nature museum located on the Burlington waterfront in northern Vermont.',
 			comments: [
 				{
 					id: 0,
@@ -139,12 +139,12 @@ const DISHES =
 		},
 		{
 			id: 3,
-			name:'ElaiCheese Cake',
-			image: 'assets/images/elaicheesecake.png',
-			category: 'dessert',
-			label:'',
-			price:'2.99',
-			description:'A delectable, semi-sweet New York Style Cheese Cake, with Graham cracker crust and spiced with Indian cardamoms',
+			name:'Engineering Ministries International',
+			image: 'assets/images/emi_bg.jpg',
+			category: 'appetizer',
+			label:'New',
+			website:'https://emiworld.org/civicrm/contribute/transact?reset=1&id=4&custom_254=Staff',
+			description:'ECHO, Leahy Center for Lake Champlain, formerly the Lake Champlain Basin Science Center, is an innovative science and nature museum located on the Burlington waterfront in northern Vermont.',
 			comments: [
 				{
 					id: 0,
@@ -162,21 +162,21 @@ const DISHES =
 				},
 				{
 					id: 2,
-					rating: 5,
+					rating: 3,
 					comment: "Eat it, just eat it!",
 					author: "Michael Jaikishan",
 					date: "2015-02-13T17:57:28.556094Z"
 				},
 				{
 					id: 3,
-					rating: 5,
+					rating: 4,
 					comment: "Ultimate, Reaching for the stars!",
 					author: "Ringo Starry",
 					date: "2013-12-02T17:57:28.556094Z"
 				},
 				{
 					id: 4,
-					rating: 5,
+					rating: 2,
 					comment: "It's your birthday, we're gonna party!",
 					author: "25 Cent",
 					date: "2011-12-02T17:57:28.556094Z"
